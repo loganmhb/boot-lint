4aaf431024e602d4787ec31d5644bce538eceb5b
Logan Buckley <lbuckley@opinionlab.com>
2016-12-14 07:58:36 -0500

    Add ancient linter

--- src/com/bckly/boot_lint.clj
+++ src/com/bckly/boot_lint.clj
@@ -3,16 +3,43 @@
             [boot.pod :as pod]
             [clojure.java.io :as io]))
 
-(deftask test-task []
-  (boot/with-pre-wrap fileset
-    (println (map boot/tmp-file (boot/by-ext [".clj"] (boot/input-files fileset))))
-    fileset))
 
 (defn clj-files [fileset]
   (->> fileset
        boot/input-files
        (boot/by-ext [".clj"])))
 
+
+(defn check-ancient [pod deps opts]
+  (pod/with-eval-in pod
+    (require '[ancient-clj.core :as ancient])
+    (let [outdated (filter #(ancient/artifact-outdated? % ~opts)
+                           '~deps)]
+      (when (seq outdated)
+        (doseq [dep outdated]
+          (println dep "is outdated:"
+                   (:version-string (ancient/latest-version! dep ~opts))
+                   "is available." ))
+        outdated))))
+
+(deftask ancient [s snapshots bool "Include snapshot versions"
+                  q qualified bool "Include qualified (e.g. alpha) versions"]
+  (boot.core/with-pre-wrap fileset
+    (let [pod (pod/make-pod (update (boot.core/get-env)
+                                    :dependencies
+                                    (partial into)
+                                    '[[ancient-clj "0.3.14"]
+                                      [slingshot "0.12.2"]]))
+          report (check-ancient pod (:dependencies (boot.core/get-env))
+                                {:snapshots? snapshots
+                                 :qualified? qualified
+                                 :repositories (:repositories (boot.core/get-env))})
+          new-meta (assoc-in (meta fileset)
+                             [::reports ::ancient]
+                             report)]
+      (if report
+        (with-meta fileset new-meta)
+        fileset))))
 
 (defn check-kibit [pod file]
   (pod/with-eval-in pod
